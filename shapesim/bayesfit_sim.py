c8c719edb654e23bb095a5190d416ae53932840b
Erin Sheldon <erin.sheldon@gmail.com>
2012-10-06 10:37:03 -0400

    redoing the man emceefitter class

--- shapesim/bayesfit_sim.py
+++ shapesim/bayesfit_sim.py
@@ -61,7 +61,7 @@ TODO:
             gg10r10  fixcen still.  Looks like shit, maybe because
             of the e guess.  Using same as other, zero centered.  Still shit.
                 re-running 0 1 using new EmceeFitterFixCen class made
-                from the mcmcfitter
+                from the mcmcfitter; looks better, re-running all now
 
         - if that checks out we can try other things like not
         fixing center (marginilizing amplitude? probably lower priority)
@@ -427,9 +427,22 @@ class BayesFitSim(shapesim.BaseSim):
                                                   burnin=self['burnin'],
                                                   when_prior=self['when_prior'])
 
-                else:
+                elif True:
                     # width of prior on center
-                    cenprior=CenPrior([res['row'],res['col']], [1.e-3]*2)
+                    cenprior=CenPrior(ci['cen'], [1.e-3]*2)
+                    self.fitter=EmceeFitter(ci.image,
+                                            1./ci['skysig'],
+                                            psf,
+                                            cenprior,
+                                            T,
+                                            self.gprior,
+                                            nwalkers=nwalkers,
+                                            nstep=self['nstep'], 
+                                            burnin=self['burnin'],
+                                            when_prior=self['when_prior'])
+                else:
+                    #deprecated
+                    cenprior=CenPrior(ci['cen'], [1.e-3]*2)
                     npars=5
                     estart=2
                     
@@ -533,7 +546,7 @@ class BayesFitSim(shapesim.BaseSim):
 
 
 
-class EmceeFitter:
+class EmceeFitterOld:
     """
     likelihood is exp(0.5*A*B^2)
 
@@ -1026,6 +1039,339 @@ class EmceeFitter:
         tab.show()
 
 
+class EmceeFitter:
+    def __init__(self, 
+                 image, 
+                 ierr, 
+                 psf,
+                 cenprior,
+                 T,
+                 gprior,
+                 nwalkers=10,
+                 nstep=100, 
+                 burnin=400,
+                 when_prior='during'):
+        """
+        parameters
+        ----------
+        image:
+            image as a numpy array
+        ierr:
+            Error per pixel
+        psf:
+            The psf gaussian mixture
+        cenprior:
+            The center prior object.
+        T:
+            Starting value for ixx+iyy of main component
+        gprior:
+            The prior on the g1,g2 surface.
+        nstep:
+            Number of steps in MCMC chain.
+        burnin:
+            Number of burn in steps.
+        when_prior:
+            'during' or 'after'
+        """
+        
+        self.make_plots=True
+
+        # cen1,cen2,e1,e2,T
+        self.npars=5
+
+        self.image=image
+        self.ierr=float(ierr)
+        self.T=T
+        self.cenprior=cenprior
+
+        self.nwalkers=nwalkers
+        self.nstep=nstep
+        self.burnin=burnin
+        self.gprior=gprior
+        self.when_prior=when_prior
+
+        self._set_psf(psf)
+
+        self.Anorm = float(1)
+
+        self.tpars=zeros(6,dtype='f8')
+
+        self._go()
+
+    def get_result(self):
+        return self._result
+
+    def _go(self):
+        import emcee
+
+        sampler = emcee.EnsembleSampler(self.nwalkers, 
+                                        self.npars, 
+                                        self._calc_lnprob,
+                                        a=2.5)
+        
+        guess=self._get_guess()
+
+        pos, prob, state = sampler.run_mcmc(guess, self.burnin)
+        sampler.reset()
+        pos, prob, state = sampler.run_mcmc(pos, self.nstep)
+
+        self.trials  = sampler.flatchain
+        lnprobs = sampler.lnprobability.reshape(self.nwalkers*self.nstep)
+        self.lnprobs = lnprobs - lnprobs.max()
+
+        self._emcee_sampler=sampler
+
+        # get the expectation values, sensitivity and errors
+        self._calc_result()
+
+        if self.make_plots:
+            self._doplots()
+            key=raw_input('hit a key (q to quit): ')
+            if key=='q':
+                stop
+
+
+    def _calc_lnprob(self, pars):
+        """
+        pars are [g1,g2,T]
+        """
+        # hard priors first
+        g1,g2=pars[2],pars[3]
+        T=pars[4]
+
+        e1,e2,ok = g1g2_to_e1e2(g1,g2)
+        if not ok:
+            return LOWVAL
+
+        if T < 0:
+            return LOWVAL
+
+        self.tpars[0]=pars[0]
+        self.tpars[1]=pars[1]
+        self.tpars[2]=e1
+        self.tpars[3]=e2
+        self.tpars[4]=T
+        self.tpars[5]=1.
+
+        logprob = self._get_loglike_c(self.tpars)
+
+        if self.when_prior=='during':
+            gp = self._get_lngprior(g1,g2)
+            logprob += gp
+
+        return logprob
+
+    def _get_loglike_c(self, pars):
+        """
+        pars is *full*
+        """
+        #t0=time.time()
+
+        loglike,flags=\
+            gmix_image.render._render.loglike_coellip(self.image, 
+                                                      pars, 
+                                                      self.psf_pars, 
+                                                      self.Anorm,
+                                                      self.ierr)
+        if flags != 0:
+            return LOWVAL
+        #self.logl_tm += time.time()-t0
+        return loglike
+
+    def _get_lngprior(self, g1, g2):
+        g=sqrt(g1**2 + g2**2)
+        gp = self.gprior.prior_gabs_scalar(g)
+        if gp > 0:
+            gp = log(gp)
+        else:
+            gp=LOWVAL
+        return gp
+
+    def _calc_result(self):
+        """
+        if when_prior='after', We apply the shape prior here
+
+        We marginalize over all parameters but g1,g2, which
+        are index 0 and 1 in the pars array
+        """
+        import mcmc
+
+        g=zeros(2)
+        gcov=zeros((2,2))
+        gsens = zeros(2)
+
+        g1vals=self.trials[:,2]
+        g2vals=self.trials[:,3]
+
+        prior = self.gprior(g1vals,g2vals)
+        dpri_by_g1 = self.gprior.dbyg1(g1vals,g2vals)
+        dpri_by_g2 = self.gprior.dbyg2(g1vals,g2vals)
+
+        psum = prior.sum()
+
+        if self.when_prior=='after':
+            # we need to multiply each by the prior
+            g[0] = (g1vals*prior).sum()/psum
+            g[1] = (g2vals*prior).sum()/psum
+
+            g1diff = g[0]-g1vals
+            g2diff = g[1]-g2vals
+
+            gcov[0,0] = (g1diff**2*prior).sum()/psum
+            gcov[0,1] = (g1diff*g2diff*prior).sum()/psum
+            gcov[1,0] = gcov[0,1]
+            gcov[1,1] = (g2diff**2*prior).sum()/psum
+
+            # now the sensitivity is 
+            #  sum( (<g>-g) L*dP/dg )
+            #  ----------------------
+            #        sum(L*P)
+            #
+            # the likelihood is already in the points
+
+            gsens[0] = 1. - (g1diff*dpri_by_g1).sum()/psum
+            gsens[1] = 1. - (g2diff*dpri_by_g2).sum()/psum
+        else:
+            # prior is already in the distribution of
+            # points.  This is simpler for most things but
+            # for sensitivity we need a factor of (1/P)dP/de
+            g, gcov = mcmc.extract_stats(self.trials[:,2:2+2], 0)
+
+            g1diff = g[0]-g1vals
+            g2diff = g[1]-g2vals
+
+            w,=where(prior > 0)
+            if w.size == 0:
+                raise ValueError("no prior values > 0!")
+
+            gsens[0]= 1.-(g1diff[w]*dpri_by_g1[w]/prior[w]).mean()
+            gsens[1]= 1.-(g2diff[w]*dpri_by_g2[w]/prior[w]).mean()
+
+ 
+        arates = self._emcee_sampler.acceptance_fraction
+        arate = arates.mean()
+        #print 'acceptance rate:',w.size/float(self.trials.size)
+        self._result={'g':g,'gcov':gcov,'gsens':gsens,'arate':arate}
+        #wlog("arate:",self._result['arate'])
+
+
+
+    def _get_guess(self):
+        guess=zeros( (self.nwalkers,self.npars) )
+
+        guess[:,0]=self.cenprior.cen[0] + 0.01*(randu(self.nwalkers)-0.5)
+        guess[:,1]=self.cenprior.cen[1] + 0.01*(randu(self.nwalkers)-0.5)
+
+        # guess for g1,g2 is (0,0) with some scatter
+        guess[:,2]=0.1*(randu(self.nwalkers)-0.5)
+        guess[:,3]=0.1*(randu(self.nwalkers)-0.5)
+
+        # guess for T is self.T with scatter
+        guess[:,4] = self.T + self.T*0.1*(randu(self.nwalkers)-0.5)
+
+        return guess
+
+    def _set_psf(self, psf):
+        self.psf_gmix = psf
+        self.psf_pars = None
+
+        if psf is not None:
+            if not isinstance(psf[0],dict):
+                raise ValueError("psf must be list of dicts")
+            self.psf_pars = gmix_image.gmix2pars(psf)
+
+
+    def _doplots(self):
+
+        import mcmc
+        import biggles
+        biggles.configure("default","fontsize_min",1.2)
+        tab=biggles.Table(5,2)
+
+        cen1vals=self.trials[:,0]
+        cen2vals=self.trials[:,1]
+        g1vals=self.trials[:,2]
+        g2vals=self.trials[:,3]
+        Tvals=self.trials[:,4]
+
+        ind=numpy.arange(g1vals.size)
+
+        burn_cen=biggles.FramedPlot()
+        cen1p=biggles.Curve(ind, cen1vals, color='blue')
+        cen2p=biggles.Curve(ind, cen2vals, color='red')
+        cen1p.label=r'$x_1$'
+        cen2p.label=r'$x_2$'
+        burn_cen.add(cen1p)
+        burn_cen.add(cen2p)
+        key=biggles.PlotKey(0.9,0.9,[cen1p,cen2p],halign='right')
+        burn_cen.add(key)
+        burn_cen.ylabel='cen'
+
+        burn_g1=biggles.FramedPlot()
+        burn_g1.add(biggles.Curve(ind, g1vals))
+        burn_g1.ylabel=r'$g_1$'
+
+        burn_g2=biggles.FramedPlot()
+        burn_g2.add(biggles.Curve(ind, g2vals))
+        burn_g2.ylabel=r'$g_2$'
+
+        burn_T=biggles.FramedPlot()
+        burn_T.add(biggles.Curve(ind, Tvals))
+        burn_T.ylabel='T'
+
+        likep = biggles.FramedPlot()
+        likep.add( biggles.Curve(ind, self.lnprobs) )
+        likep.ylabel='ln( prob )'
+
+
+        g = self._result['g']
+        gcov = self._result['gcov']
+        errs = sqrt(diag(gcov))
+
+        res=self.get_result()
+        print 'acceptance rate:',res['arate']
+        print 'g1: %.16g +/- %.16g' % (g[0],errs[0])
+        print 'g2: %.16g +/- %.16g' % (g[1],errs[1])
+        print 'g1sens:',self._result['gsens'][0]
+        print 'g2sens:',self._result['gsens'][1]
+
+        cenw = cen1vals.std()
+        cen_bsize=cenw*0.2
+        hplt_cen0 = eu.plotting.bhist(cen1vals,binsize=cen_bsize,
+                                      color='blue',
+                                      show=False)
+        hplt_cen = eu.plotting.bhist(cen2vals,binsize=cen_bsize,
+                                     color='red',
+                                     show=False, plt=hplt_cen0)
+        hplt_cen.add(key)
+
+        bsize1=errs[0]*0.2
+        bsize2=errs[1]*0.2
+        hplt_g1 = eu.plotting.bhist(g1vals,binsize=bsize1,
+                                  show=False)
+        hplt_g2 = eu.plotting.bhist(g2vals,binsize=bsize2,
+                                  show=False)
+
+        Tsdev = Tvals.std()
+        Tbsize=Tsdev*0.2
+        hplt_T = eu.plotting.bhist(Tvals,binsize=Tbsize,
+                                  show=False)
+
+        hplt_g1.xlabel=r'$g_1$'
+        hplt_g2.xlabel=r'$g_2$'
+        hplt_T.xlabel='T'
+        tab[0,0] = burn_cen
+        tab[1,0] = burn_g1
+        tab[2,0] = burn_g2
+        tab[3,0] = burn_T
+
+        tab[0,1] = hplt_cen
+        tab[1,1] = hplt_g1
+        tab[2,1] = hplt_g2
+        tab[3,1] = hplt_T
+        tab[4,0] = likep
+        tab.show()
+
 class EmceeFitterFixCen:
     def __init__(self, 
                  image, 
@@ -1088,12 +1434,6 @@ class EmceeFitterFixCen:
 
     def get_result(self):
         return self._result
-
-    def _go(self):
-        if self.sampler=='mcmc':
-            self._go_mcmc()
-        else:
-            self._go_emcee()
 
     def _go(self):
         import emcee
