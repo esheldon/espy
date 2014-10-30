2eba663066d3f3687df7730fdb90a50282e57c49
Erin Sheldon <erin.sheldon@gmail.com>
2014-10-30 09:15:11 -0400

    docs

--- des/files_common.py
+++ des/files_common.py
@@ -2,17 +2,26 @@ from __future__ import print_function
 import os
 
 def config_dir():
+    """
+    dir holding config files
+    """
     d=os.environ['ESPY_DIR']
     d=os.path.join(d, 'des', 'config')
     return d
 
 def config_file(name):
+    """
+    config file name
+    """
     d=config_dir()
     name='%s.yaml' % name
 
     return os.path.join(d, name)
 
 def read_config(name):
+    """
+    read the config file
+    """
     import yaml
     fname=config_file(name)
     print("reading:",fname)
@@ -29,11 +38,16 @@ def get_lensdir():
     return os.environ['LENSDIR']
 
 def get_cat_basedir():
+    """
+    base dir holding all catalog dirs
+    """
     catdir = os.path.join(get_lensdir(), 'catalogs')
     return catdir
 
-
 def get_cat_dir(catname):
+    """
+    dir holding a catalog type
+    """
     d=get_cat_basedir()
     d=os.path.join(d, catname)
     return d
