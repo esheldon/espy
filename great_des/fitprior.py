ae6ad8a017a6cbeaad394a1cb388402551dbd202
Erin Sheldon <erin.sheldon@gmail.com>
2015-02-20 08:24:04 -0500

    fitting great des noisefree data for g prior

--- great_des/fitprior.py
+++ great_des/fitprior.py
@@ -2,23 +2,23 @@ from __future__ import print_function, division
 import os
 import numpy
 
-def fit_prior(show=False, gmax=0.975):
+def fit_prior(show=False, gmax_data=0.975, gmax=1.0):
     import esutil as eu
     import ngmix
 
-    g=cache_data(gmax)
+    g=cache_data(gmax_data)
     hd=eu.stat.histogram(g, nbin=100, min=0, max=gmax,more=True)
 
     xdata=hd['center']
     ydata=hd['hist'].astype('f8')
 
     #pg3=ngmix.priors.GPriorGreat3Exp()
-    #pg3=ngmix.priors.GPriorGreatDES(gmax=gmax)
-    pg3=ngmix.priors.GPriorGreatDES2(gmax=gmax)
+    pg3=ngmix.priors.GPriorGreatDES(gmax=gmax)
+    #pg3=ngmix.priors.GPriorGreatDES2(gmax=gmax)
 
     pg3.dofit(hd['center'], hd['hist'].astype('f8'), show=show)
 
-def cache_data(gmax):
+def cache_data(gmax_data):
     import fitsio
     tmpdir=os.environ['TMPDIR']
     cachename=os.path.join(tmpdir,'sfit-noisefree-02-g00-gvals.fits')
@@ -42,6 +42,6 @@ def cache_data(gmax):
         print("writing:",cachename)
         fitsio.write(cachename, out, clobber=True)
 
-    w,=numpy.where(g < 0.99)
+    w,=numpy.where(g < gmax_data)
     g=g[w]
     return g
