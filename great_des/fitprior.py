d35dfb14d94e41e0890c85843a50033d65787538
Erin Sheldon <erin.sheldon@gmail.com>
2015-02-19 19:52:35 -0500

    new configs

---
+++ great_des/fitprior.py
@@ -0,0 +1,23 @@
+from __future__ import print_function, division
+import numpy
+
+def fit_prior(show=False):
+    import esutil as eu
+    import ngmix
+    import fitsio
+
+    fname='/astro/u/esheldon/lensing/great-des/sfit-noisefree-02/collated/sfit-noisefree-02-g00.fits'
+    print("reading:",fname)
+    t=fitsio.read(fname)
+
+    g2 = numpy.sqrt( t['g'][:,0]**2 + t['g'][:,1]**2 )
+    w,=numpy.where(t['flags']==0)
+
+    hd=eu.stat.histogram(g2[w], nbin=100, min=0, max=0.985,more=True)
+
+    xdata=hd['center']
+    ydata=hd['hist'].astype('f8')
+
+    pg3=ngmix.priors.GPriorGreat3Exp()
+
+    pg3.dofit_lm(hd['center'], hd['hist'].astype('f8'), show=show)
