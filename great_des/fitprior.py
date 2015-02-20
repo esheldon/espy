96774eb435485b2d933b3bf4c0bcddd7577bce2e
Erin Sheldon <erin.sheldon@gmail.com>
2015-02-19 23:18:21 -0500

    workin gon prior fitting

--- great_des/fitprior.py
+++ great_des/fitprior.py
@@ -2,22 +2,23 @@ from __future__ import print_function, division
 import os
 import numpy
 
-def fit_prior(show=False):
+def fit_prior(show=False, gmax=0.975):
     import esutil as eu
     import ngmix
 
-    g=cache_data()
-    hd=eu.stat.histogram(g, nbin=100, min=0, max=0.985,more=True)
+    g=cache_data(gmax)
+    hd=eu.stat.histogram(g, nbin=100, min=0, max=gmax,more=True)
 
     xdata=hd['center']
     ydata=hd['hist'].astype('f8')
 
     #pg3=ngmix.priors.GPriorGreat3Exp()
-    pg3=ngmix.priors.GPriorGreatDES(gmax=0.985)
+    #pg3=ngmix.priors.GPriorGreatDES(gmax=gmax)
+    pg3=ngmix.priors.GPriorGreatDES2(gmax=gmax)
 
     pg3.dofit(hd['center'], hd['hist'].astype('f8'), show=show)
 
-def cache_data():
+def cache_data(gmax):
     import fitsio
     tmpdir=os.environ['TMPDIR']
     cachename=os.path.join(tmpdir,'sfit-noisefree-02-g00-gvals.fits')
@@ -41,4 +42,6 @@ def cache_data():
         print("writing:",cachename)
         fitsio.write(cachename, out, clobber=True)
 
+    w,=numpy.where(g < 0.99)
+    g=g[w]
     return g
