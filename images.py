dac9a7fe161cba2c8401f039744c76060332d92b
Erin Sheldon <erin.sheldon@gmail.com>
2015-06-19 20:34:17 -0400

    pass on sizes

--- images.py
+++ images.py
@@ -439,7 +439,9 @@ def compare_images(im1, im2, **keys):
         tab[1,0] = residplt
 
     if show:
-        tab.show()
+        width=keys.get('width',None)
+        height=keys.get('height',None)
+        tab.show(width=width, height=height)
 
     biggles.configure( 'default', 'fontsize_min', 1.25)
 
